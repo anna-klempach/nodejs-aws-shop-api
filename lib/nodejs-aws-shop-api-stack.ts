f67e8340eb1c723e5e46b4405ea1511f6635f583
Hanna Klempach <hklempach@wideorbit.com>
2023-06-12 15:06:25 +0200

    task-4 / Integrate lambdas with db

--- lib/nodejs-aws-shop-api-stack.ts
+++ lib/nodejs-aws-shop-api-stack.ts
@@ -2,24 +2,48 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { ErrorSchema, ProductListSchema, ProductSchema } from '../src/models';
 import { CORS_PREFLIGHT_SETTINGS } from '../src/utils';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 export class NodejsAwsShopApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const tableReadRole = new iam.Role(this, 'LambdaDynamoDBReadRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    const readPolicyStatement = new iam.PolicyStatement({
+      actions: ['dynamodb:GetItem', 'dynamodb:Scan'],
+      resources: [process.env.STOCKS_TABLE_ARN || '', process.env.PRODUCTS_TABLE_ARN || ''],
+    });
+    tableReadRole.addToPolicy(readPolicyStatement);
+
+    const COMMON_LAMBDA_PROPS: Partial<NodejsFunctionProps> = {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      timeout: cdk.Duration.seconds(300),
+      role: tableReadRole,
+      environment: {
+        PRODUCTS_TABLE_NAME: process.env.PRODUCTS_TABLE_NAME || '',
+        STOCKS_TABLE_NAME: process.env.STOCKS_TABLE_NAME || '',
+        REGION: process.env.REGION || ''
+      }
+    }
     const getProductsList = new NodejsFunction(this, 'GetProductsListHandler', {
       functionName: 'getProductsList',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      entry: 'src/lambda/get-products-list.ts'
+      entry: 'src/lambda/get-products-list.ts',
+      ...COMMON_LAMBDA_PROPS
     });
 
     const getProductsById = new NodejsFunction(this, 'GetProductsByIdHandler', {
       functionName: 'getProductsById',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      entry: 'src/lambda/get-products-by-id.ts'
+      entry: 'src/lambda/get-products-by-id.ts',
+      ...COMMON_LAMBDA_PROPS
     });
 
     const api = new apigw.RestApi(this, 'products-api', {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { AttributeValue, TransactGetItemsCommandInput } from '@aws-sdk/client-dynamodb';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export const CORS_HEADERS = {
@@ -19,3 +20,57 @@ export const CORS_PREFLIGHT_SETTINGS = {
   allowHeaders: ['*'],
   allowMethods: apigw.Cors.ALL_METHODS
 };
+
+export const PRODUCTS_SCAN_INPUT = {
+  TableName: "products",
+};
+
+export const STOCKS_SCAN_INPUT = {
+  TableName: "stocks",
+};
+
+export const getTransactItemInput = (productId: string): TransactGetItemsCommandInput => {
+  return {
+    TransactItems: [
+      {
+        Get: {
+          TableName: process.env.PRODUCTS_TABLE_NAME,
+          Key: {
+            id: {
+              S: productId,
+            }
+          }
+        },
+      },
+      {
+        Get: {
+          TableName: process.env.STOCKS_TABLE_NAME,
+          Key: {
+            product_id: {
+              S: productId,
+            }
+          }
+        },
+      },
+    ]
+  }
+};
+
+export const joinDataList = (productData: Array<Record<string, AttributeValue>>, stockData: Array<Record<string, AttributeValue>>) => {
+  return productData.map((product) => {
+    const productId = product.id.S;
+    const matchingStock = stockData.find(
+      (stock) => stock.product_id.S === productId
+    );
+    return joinData(product, matchingStock);
+  });
+};
+
+export const joinData = (product: Record<string, AttributeValue>, stock?: Record<string, AttributeValue>) => ({
+  id: product.id.S,
+  title: product.title.S,
+  description: product.description.S,
+  price: Number(product.price.N),
+  count: stock ? Number(stock.count.N) : 0,
+});
+
